bd90df71b75236caa4b789cf8ca7e6af7c2cf5a3
Murilo Pereira <bupereira@gmail.com>
2022-09-14 22:58:09 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Component01Component } from './component01/component01.component';
+import { Component02Component } from './component02/component02.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component01Component,
+    Component02Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
