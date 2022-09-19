675a13ad55cc13d0f812aa0c4dfc6c1a10db8fbf
Murilo Pereira <bupereira@gmail.com>
2022-09-18 23:27:11 -0300

    Adicionando mapeamento de rotas, navegacao por meio de rotas declaradas, passagem de dados via hierarquia do app pro footer, e passando dados entre componentes que representam diferentes telas via parametro de rotas, utilizando ngIf e passando a funcao como parametro necessario da rota

---
+++ src/app/cadastroeleitores/cadastroeleitores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastroeleitores',
+  templateUrl: './cadastroeleitores.component.html',
+  styleUrls: ['./cadastroeleitores.component.css']
+})
+export class CadastroeleitoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
