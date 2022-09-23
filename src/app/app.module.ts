60228e6a42ff74c5d82605c9a454c4df6db810bf
Murilo Pereira <bupereira@gmail.com>
2022-09-23 03:14:51 -0300

    Adding RegEx validation, disabling button until form is valid and having an entity using localStorage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +24,8 @@ import { CadastroeleitoresComponent } from './cadastroeleitores/cadastroeleitore
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastrozonas/cadastrozonas.component.ts
+++ src/app/cadastrozonas/cadastrozonas.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { ZonasStorageService } from './zonas-storage.service';
+import { Component, OnInit, } from '@angular/core';
+import { Zona } from '../model/zona';
+import { Shared } from '../utils/shared';
 
 @Component({
   selector: 'app-cadastrozonas',
   templateUrl: './cadastrozonas.component.html',
-  styleUrls: ['./cadastrozonas.component.css']
+  styleUrls: ['./cadastrozonas.component.css'],
+  providers: [ZonasStorageService]
 })
 export class CadastrozonasComponent implements OnInit {
+  //zonas?: Zona[];
+  get zonas(): Zona[] {
+    return this.zonaService.getZonas();
+  };
+  zona!: Zona;
+  showMessage: boolean = false;
+  success!: boolean;
+  message!: string;
 
-  constructor() { }
+  constructor(private zonaService: ZonasStorageService) {
+  }
 
   ngOnInit(): void {
+    Shared.initializeWebStorage();
+    this.zona = new Zona("", "");
+    //this.zonas = this.zonaService.getZonas();
+  }
+
+  onSubmit() {
+    if (!this.zonaService.exists(this.zona.codZona)) {
+      this.zonaService.save(this.zona);
+    } else {
+      this.zonaService.update(this.zona);
+    }
+    alert("Zona Eleitoral Adicionada.")
+  }
+
+  onEdit(zona: Zona) {
+    let clone = Zona.clone(zona);
+    this.zona = clone;
+  }
+
+  onDelete(codZona: string) {
+    let confirm = window.confirm(
+      'Tem certeza de que deseja excluir a zona ' + codZona
+    );
+    if (!confirm) {
+      return;
+    }
+    let response: boolean = this.zonaService.delete(codZona);
+    this.showMessage = true;
+    this.success = response;
+    if (response) {
+      this.message = 'Zona excluida com sucesso!';
+    } else {
+      this.message = 'Falha ao excluir a zona eleitoral.';
+    }
+    //this.zonas = this.zonaService.getZonas();
+
   }
 
 }
